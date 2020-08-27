80015c71f2bcfb927f203170aba50a8948c29ca1
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-27 00:54:00 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,10 +1,17 @@
-
-
-public class Product{
-  static int x = 25;
-  static int y = 5;
- public static void main(String[] args)
-  {
-   System.out.println(x*y);
-   }
+import java.util.Scanner;
+ 
+public class Product {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input #1: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Input #2: ");
+  int num2 = in.nextInt();
+   
+  System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+ }
+ 
 }
