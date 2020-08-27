09dfc12cd153e951d11a27f17521d4d581bb73ca
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-27 00:56:34 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Product {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input #1: ");
+  int digit1 = in.nextInt();
+   
+  System.out.print("Input #2: ");
+  int digit2 = in.nextInt();
+   
+  System.out.println(digit1 + " x " + digit2 + " = " + digit1 * digit2);
+ }
+ 
+}
